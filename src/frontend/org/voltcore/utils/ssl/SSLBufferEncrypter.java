f18a46c04624be1f9f6cd8c08af312d15ee1963f
Nate Clark <nclark@voltdb.com>
2019-05-24 11:05:26 -0400

    ENG-15983: Do not use CompositeByteBuf to collect output
    Simplify encryption path and prevent building large heap buffers.
    CompositByteBuf will combine buffers once it has more than a certain number of
    buffers within it. When discardReadComponents() is called it will only discard
    a the last buffer if it has no more space to write data. This means it is
    possible to have a really large buffer in the CompositeByteBuf which will never
    be used.
    
    Remove slicing logic since it really is not necessary. Instead call encrypt
    multiple times with the same input and output buffer until all data is
    encrypted. Instead of collecting all of the encrypted buffers to send at once
    send each buffer individually.

--- src/frontend/org/voltcore/utils/ssl/SSLBufferEncrypter.java
+++ src/frontend/org/voltcore/utils/ssl/SSLBufferEncrypter.java
@@ -23,47 +23,75 @@ import java.nio.ReadOnlyBufferException;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLEngineResult;
 import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
 
 import org.voltcore.network.TLSException;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.Unpooled;
 
 public class SSLBufferEncrypter {
 
     private final SSLEngine m_sslEngine;
 
-    public SSLBufferEncrypter(SSLEngine sslEngine, int applicationBufferSize, int packetBufferSize) {
-        this.m_sslEngine = sslEngine;
-    }
-
     public SSLBufferEncrypter(SSLEngine sslEngine) {
         this.m_sslEngine = sslEngine;
     }
 
-    public void tlswrap(ByteBuffer srcBuffer, ByteBuffer dstBuffer)  {
-        while (true) {
-            SSLEngineResult result = null;
-            try {
-                result = m_sslEngine.wrap(srcBuffer, dstBuffer.slice());
-            } catch (SSLException|ReadOnlyBufferException|IllegalArgumentException|IllegalStateException e) {
-                throw new TLSException("ssl engine wrap fault", e);
-            }
-            switch (result.getStatus()) {
+    public ByteBuf tlswrap(ByteBuffer src, ByteBufAllocator allocator) {
+        ByteBuf encrypted = tlswrap(Unpooled.wrappedBuffer(src), allocator);
+        src.position(src.limit());
+        return encrypted;
+    }
+
+    public ByteBuf tlswrap(ByteBuf src, ByteBufAllocator allocator) {
+        SSLSession session = m_sslEngine.getSession();
+        int packetBufferSize = session.getPacketBufferSize();
+        /*
+         * Netty OpenSSL implementation can return larger application buffer size than packet buffer size. Use formula
+         * from sun.security.ssl.SSLSessionImpl.getApplicationBufferSize() to calculate application buffer size from
+         * packet buffer size.
+         */
+        int applicationBufferSize = Math.min(session.getApplicationBufferSize(), packetBufferSize - 5);
+        int toEncrypt = src.readableBytes();
+        int maxEncryptedSize = ((toEncrypt + applicationBufferSize - 1) / applicationBufferSize)
+                * packetBufferSize;
+
+        ByteBuf dest = allocator.buffer(maxEncryptedSize);
+
+        try {
+            assert dest.nioBufferCount() == 1 : "Should only have one buffer: " + dest.nioBufferCount();
+            ByteBuffer destNioBuf = dest.nioBuffer(dest.writerIndex(), dest.writableBytes());
+
+            ByteBuffer[] srcNioBuffers = src.nioBuffers();
+            do {
+                SSLEngineResult result = null;
+                try {
+                    result = m_sslEngine.wrap(srcNioBuffers, destNioBuf);
+                } catch (SSLException | ReadOnlyBufferException | IllegalArgumentException | IllegalStateException e) {
+                    throw new TLSException("ssl engine wrap fault", e);
+                }
+                switch (result.getStatus()) {
                 case OK:
-                    // in m_dstBuffer, newly decrtyped data is between pos and lim
-                    if (result.bytesProduced() > 0) {
-                        dstBuffer.limit(dstBuffer.position() + result.bytesProduced());
-                        return;
-                        }
-                    else {
-                        continue;
-                    }
+                    src.readerIndex(src.readerIndex() + result.bytesConsumed());
+                    break;
                 case BUFFER_OVERFLOW:
-                    throw new TLSException("SSL engine unexpectedly overflowed when enrypting");
+                    throw new TLSException(String.format(
+                            "SSL engine unexpectedly overflowed when encrypting: encrypting %d bytes did not fit in %s",
+                            toEncrypt, destNioBuf));
                 case BUFFER_UNDERFLOW:
                     throw new TLSException("SSL engine unexpectedly underflowed when encrypting");
                 case CLOSED:
                     throw new TLSException("SSL engine is closed on ssl wrap of buffer.");
-            }
+                }
+            } while (src.isReadable());
+
+            dest.writerIndex(destNioBuf.position());
+            return dest;
+        } catch (Throwable t) {
+            dest.release();
+            throw t;
         }
     }
-
 }
